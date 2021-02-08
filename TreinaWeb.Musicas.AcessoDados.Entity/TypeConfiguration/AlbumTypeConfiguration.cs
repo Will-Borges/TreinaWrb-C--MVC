736ed0f37b676c0f04faa404a02ae97e511e4898
William Borges <36491847+Will-Borges@users.noreply.github.com>
2021-02-08 00:48:34 -0300

    Primeito Git

---
+++ TreinaWeb.Musicas.AcessoDados.Entity/TypeConfiguration/AlbumTypeConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinaWeb.Comum.Entity;
+using TreinaWeb.Musicas.Dominio;
+
+namespace TreinaWeb.Musicas.AcessoDados.Entity.TypeConfiguration
+{
+    public class AlbumTypeConfiguration : TreinaWebEntityAbstractConfig<Album>
+    {
+        protected override void ConfigurarCamposTabela()
+        {
+            Property(p => p.Id)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                .HasColumnName("ALB_ID");
+
+            Property(P => P.Nome)
+                .IsRequired()
+                .HasColumnName("ALB_NOME")
+                .HasMaxLength(100);
+
+            Property(P => P.Ano)
+                .IsRequired()
+                .HasColumnName("ALB_ANO");
+
+            Property(P => P.Observacoes)
+                .IsOptional()
+                .HasColumnName("ALB_OBSERVACOES")
+                .HasMaxLength(500);
+        }
+
+        protected override void ConfigurarChavePrimaria()
+        {
+            HasKey(pk => pk.Id);
+        }
+
+        protected override void ConfigurarChavesEstrangeiras()
+        {
+            
+        }
+
+        protected override void ConfigurarNomeTabela()
+        {
+            ToTable("ALB_ALBUNS");
+        }
+    }
+}
